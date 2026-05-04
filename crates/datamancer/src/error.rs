33c46fd39c27a8a1b492e5ef5276a649cf4f7670
Zach <zheylmun@gmail.com>
2026-05-04 08:58:46 +0200

    Clean up new structure

--- crates/datamancer/src/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-pub enum Error {
-    #[error("Inner state machine terminated unexpectedly")]
-    UnexpectedTermination,
-    #[error(transparent)]
-    Alpaca(#[from] oxidized_alpaca::Error),
-}
